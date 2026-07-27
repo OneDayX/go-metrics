a3d7f77ac5f69e0c64f4b4c91721825882da5808
OneNight <onenightya@yandex.ru>
2026-07-28 01:27:10 +0500

    refactor metrics collect with reflection

--- internal/service/metric_service.go
+++ internal/service/metric_service.go
@@ -4,11 +4,22 @@ import (
 	"errors"
 	"math/rand/v2"
 	"net/http"
+	"reflect"
 	"runtime"
 	"strconv"
 
 	"github.com/OneDayX/go-metrics/internal/models"
 )
+
+// runtimeMetricNames lists the runtime.MemStats gauge metric names collected by Collect().
+var runtimeMetricNames = []string{
+	"Alloc", "BuckHashSys", "Frees", "GCCPUFraction", "GCSys",
+	"HeapAlloc", "HeapIdle", "HeapInuse", "HeapObjects", "HeapReleased",
+	"HeapSys", "LastGC", "Lookups", "MCacheInuse", "MCacheSys",
+	"MSpanInuse", "MSpanSys", "Mallocs", "NextGC", "NumForcedGC",
+	"NumGC", "OtherSys", "PauseTotalNs", "StackInuse", "StackSys",
+	"Sys", "TotalAlloc",
+}
 
 // storager defines the storage contract required by the service.
 type storager interface {
@@ -39,36 +50,15 @@ func (m *MetricService) Collect() {
 	var memStats runtime.MemStats
 	runtime.ReadMemStats(&memStats)
 
-	m.Update(models.Metric{ID: "Alloc", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.Alloc))})
-	m.Update(models.Metric{ID: "BuckHashSys", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.BuckHashSys))})
-	m.Update(models.Metric{ID: "Frees", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.Frees))})
-	m.Update(models.Metric{ID: "GCCPUFraction", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.GCCPUFraction))})
-	m.Update(models.Metric{ID: "GCSys", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.GCSys))})
-	m.Update(models.Metric{ID: "HeapAlloc", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.HeapAlloc))})
-	m.Update(models.Metric{ID: "HeapIdle", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.HeapIdle))})
-	m.Update(models.Metric{ID: "HeapInuse", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.HeapInuse))})
-	m.Update(models.Metric{ID: "HeapObjects", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.HeapObjects))})
-	m.Update(models.Metric{ID: "HeapReleased", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.HeapReleased))})
-	m.Update(models.Metric{ID: "HeapSys", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.HeapSys))})
-	m.Update(models.Metric{ID: "LastGC", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.LastGC))})
-	m.Update(models.Metric{ID: "Lookups", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.Lookups))})
-	m.Update(models.Metric{ID: "MCacheInuse", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.MCacheInuse))})
-	m.Update(models.Metric{ID: "MCacheSys", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.MCacheSys))})
-	m.Update(models.Metric{ID: "MSpanInuse", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.MSpanInuse))})
-	m.Update(models.Metric{ID: "MSpanSys", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.MSpanSys))})
-	m.Update(models.Metric{ID: "Mallocs", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.Mallocs))})
-	m.Update(models.Metric{ID: "NextGC", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.NextGC))})
-	m.Update(models.Metric{ID: "NumForcedGC", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.NumForcedGC))})
-	m.Update(models.Metric{ID: "NumGC", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.NumGC))})
-	m.Update(models.Metric{ID: "OtherSys", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.OtherSys))})
-	m.Update(models.Metric{ID: "PauseTotalNs", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.PauseTotalNs))})
-	m.Update(models.Metric{ID: "StackInuse", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.StackInuse))})
-	m.Update(models.Metric{ID: "StackSys", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.StackSys))})
-	m.Update(models.Metric{ID: "Sys", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.Sys))})
-	m.Update(models.Metric{ID: "TotalAlloc", MType: models.MetricTypeGauge, Value: ptr(float64(memStats.TotalAlloc))})
+	v := reflect.ValueOf(&memStats).Elem()
+	for _, name := range runtimeMetricNames {
+		field := v.FieldByName(name)
+		value := float64(field.Int())
+		m.Update(models.Metric{ID: name, MType: models.MetricTypeGauge, Value: models.Ptr(value)})
+	}
 
-	m.Update(models.Metric{ID: "RandomValue", MType: models.MetricTypeGauge, Value: ptr(rand.Float64())})
-	m.Update(models.Metric{ID: "PollCount", MType: models.MetricTypeCounter, Delta: ptr(int64(1))})
+	m.Update(models.Metric{ID: "RandomValue", MType: models.MetricTypeGauge, Value: models.Ptr(rand.Float64())})
+	m.Update(models.Metric{ID: "PollCount", MType: models.MetricTypeCounter, Delta: models.Ptr(int64(1))})
 }
 
 func (m *MetricService) Send(host string) error {
